577b9336b0c0dbf19616785ded118b1f20b20fef
Rahul <professional7dev@gmail.com>
2025-01-15 13:55:04 +0530

    plot data learn
    till_plot_data file created

---
+++ plot_data.py
@@ -0,0 +1,17 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+health_data = pd.read_csv("data1.csv",header=0,sep=",")
+
+health_data.plot(x='Average_Pulse',y='Calorie_Burnage', kind='line')
+plt.ylim(ymin=0)
+plt.xlim(xmin=0)
+
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
